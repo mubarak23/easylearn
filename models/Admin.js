26946caf645c2d593133e2997a9eeebd57dd7e91
mubarak23 <mubarakaminu340@gmail.com>
2020-07-05 10:11:24 +0100

    admin model structure

--- models/Admin.js
+++ models/Admin.js
@@ -2,5 +2,21 @@ const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema.Types;
 
 const AdminSchema = new mongoose.Schema({
-    
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: email,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  level: {
+    type: Boolean,
+    default: 3,
+  },
+});
