1954cd4505170230873895319afab2466aaa21fd
mubarak23 <mubarakaminu340@gmail.com>
2020-07-06 09:43:23 +0100

    check on code

--- config/db.js
+++ config/db.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to DB');
+  } catch (err) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
 
-const connectDB = () =>{
-    try{
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-			useCreateIndex: true,
-			useFindAndModify: false,
-			useUnifiedTopology: true
-        })
-        console.log('Connected to DB')
-    }catch(err){
-        console.error(error.message);
-        process.exit(1)
-    }
-}
-
-module.exports = connectDB
+module.exports = connectDB;
