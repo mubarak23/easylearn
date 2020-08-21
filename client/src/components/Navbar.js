98766fdcdf81312fa6789e47dd91fd02e84ab0e4
mubarak23 <mubarakaminu340@gmail.com>
2020-08-21 16:43:59 +0100

    clear errors

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,14 +3,11 @@ import { Link, useHistory } from 'react-router-dom';
 import { userContext } from '../App';
 
 const NavBar = () => {
-   
-        
-          
     
     return (
       <nav>
         <div className='nav-wrapper'>
-          <Link to={state ? '/' : '/signin'} className='brand-logo left'>
+          <Link to='/signin' className='brand-logo left'>
             InternLike
           </Link>
           <ul id='nav-mobile' className='right'>
