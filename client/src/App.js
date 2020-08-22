fdafadc50ae8a3aaf95f9ed90ac01642d941dfc0
mubarak23 <mubarakaminu340@gmail.com>
2020-08-22 09:52:49 +0100

    connect reducer with useState

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,28 @@ import Navbar from './components/Navbar';
 import Home from './components/screens/Home';
 import Signin from './components/screens/Login';
 import Singup from './components/screens/Signup';
+import {
+  reducer,
+  intialState,
+} from '../src/components/reducers/subjectReducer';
 
 import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom';
 
+export const studentContext = createContext();
+
 const Routing = () => {
+  const history = useHistory();
+  const { state, dispatch } = useContext(studentContext);
+
+  useEffect(() => {
+    const studenttoken = JSON.parse(localStorage.getItem('token'));
+    if (studenttoken) {
+      dispatch({ type: 'STUDENT', payload: studenttoken });
+    } else {
+      history.push('/signin');
+    }
+  }, []);
+
   return (
     <Switch>
       <Route exact path='/'>
@@ -23,11 +41,14 @@ const Routing = () => {
 };
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, intialState);
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routing />
-    </BrowserRouter>
+    <studentContext.Provider value={{ state, dispatch }}>
+      <BrowserRouter>
+        <Navbar />
+        <Routing />
+      </BrowserRouter>
+    </studentContext.Provider>
   );
 }
 
