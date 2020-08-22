7bde40d93b1e331a16de798021e57993f0010814
mubarak23 <mubarakaminu340@gmail.com>
2020-08-22 10:00:51 +0100

    test dispatch

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import { studentContext } from '../../App';
 import M from 'materialize-css';
 import '../../App.css';
 
 const Login = () => {
   const history = useHistory();
+  const { state, dispatch } = useContext(studentContext);
   const [password, setPassword] = useState('');
   const [email, SetEmail] = useState('');
 
@@ -22,6 +24,13 @@ const Login = () => {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('sudent', JSON.stringify(data.token));
+        dispatch({ type: 'STUDENT', payload: data });
+        M.toast({
+          html: 'Signin Sucessfully',
+          classes: '#43a047 green darken-1',
+        });
         history.push('/home');
       });
   };
